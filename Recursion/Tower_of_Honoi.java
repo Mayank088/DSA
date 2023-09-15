4160442fc74ea35a2f6fb300e3360ef8577ee809
Mayank <136943277+Mayank088@users.noreply.github.com>
2023-09-15 18:56:47 +0530

    Add files via upload

--- Recursion/Tower_of_Honoi.java
+++ Recursion/Tower_of_Honoi.java
@@ -1,18 +1,21 @@
 package Recursion;
 
+
+//n = 3 then step to find move is (2^n)-1
 public class Tower_of_Honoi {
-    void TOH(int n, String a ,String b ,String c){
+    void TOH(int n, String A ,String B ,String C){
         if (n==1){
-            System.out.println("Move " + n + " from " + a + " to " + c);
+            System.out.println("Move " + n + " from " +  A + " to " + C);
             return;
         }
-        TOH(n-1,a,c,b);
-        System.out.println("Move " + n + " from " + a + " to " + c);
-        TOH(n-1,b,a,c);
+        TOH(n-1,A,C,B);
+        System.out.println("Move " + n + " from " + A + " to " + C);
+        TOH(n-1,B,A,C);
     }
 
     public static void main(String[] args) {
         Tower_of_Honoi t = new Tower_of_Honoi();
         t.TOH(3 ,"a","b","c");
+
     }
 }
