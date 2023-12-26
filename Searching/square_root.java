3d1b6823521afdbaf769074fe4d5298b679b27c6
Mayank <136943277+Mayank088@users.noreply.github.com>
2023-12-26 22:16:00 +0530

    Add files via upload

---
+++ Searching/square_root.java
@@ -0,0 +1,54 @@
+package Searching;
+import java.util.*;
+public class square_root {
+
+    public static int squareRoot(int x)
+    {
+        double ans = Math.sqrt(x);
+        return (int) ans;
+    }
+
+    public static int squareRoot2(int x)
+    {
+        int i=1;
+        while (i*i<=x)            //time = theta(√X)
+        {
+            i++;
+        }
+        return (i-1);
+    }
+
+    public static int squareRoot3(int x)     //time O(logn)
+    {
+        int start = 0;
+        int end = x;
+        int ans = -1;
+
+        while (start<=end)
+        {
+            int mid = (start+end)/2;
+
+            if ((mid*mid) == x)
+            {
+                return mid;
+            }
+
+            else if ((mid*mid) > x)
+            {
+                end = mid-1;
+            }
+            else  {
+                start = mid+1;
+                ans = mid;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+
+        int x = 24;
+        int ans = squareRoot3(x);
+        System.out.println(ans);
+    }
+}
