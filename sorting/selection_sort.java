fc44952965f8f583a6d9cf8b00bec74e90cdf479
Mayank <136943277+Mayank088@users.noreply.github.com>
2024-01-10 17:15:33 +0530

    Add files via upload

--- sorting/selection_sort.java
+++ sorting/selection_sort.java
@@ -1,6 +1,7 @@
 package sorting;
 
 public class selection_sort {
+
     public static void selection(int arr[])
     {
         int n = arr.length;
@@ -8,21 +9,27 @@ public class selection_sort {
         for (int i=0 ; i<n ; i++)
         {
             int min = i;
-            for (int j=i+1 ; j<n ;j++)
+            for (int j=i+1 ; j<n ; j++)
             {
                 if (arr[min] > arr[j])
                 {
                     min = j;
                 }
             }
-
-
+            int temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
         }
     }
 
 
     public static void main(String[] args)
     {
-
+        int arr[] = {5,2,90,1,54,22,30};
+        selection(arr);
+        for (int i=0 ; i<arr.length ; i++)
+        {
+            System.out.print(arr[i] + " ");
+        }
     }
 }
